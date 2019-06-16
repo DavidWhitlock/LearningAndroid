06723896f5d39b664a8459341ee174baf1ed337c
David Whitlock <david.m.whitlock@gmail.com>
2019-06-16 10:50:26 -0700

    Added files created by the Android project generator in IntelliJ (Android Studio).

---
+++ myapp/app/src/main/java/edu/pdx/cs410J/android/MainActivity.java
@@ -0,0 +1,13 @@
+package edu.pdx.cs410J.android;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
